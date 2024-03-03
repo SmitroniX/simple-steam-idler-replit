e1dc97ed653b0eeb8f41e7ecadeff66b63dba1a3
SmitroniX <142213284+SmitroniX@users.noreply.github.com>
2024-03-04 03:43:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const keep_alive = require('./keep_alive.js')
 
 var username = process.env.username;
 var password = process.env.password;
-var shared_secret = process.env.shared;
 
 var games = [730, 440, 570, 271590];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
